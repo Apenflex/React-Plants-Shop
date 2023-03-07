b2a66bd7adb2e99b6d9cbcb6546a8436e7549efd
Svyatoslav <blackdbf@gmail.com>
2023-03-07 15:57:11 +0200

    added accordion in testimonialsPage

--- src/components/pages/testimonialPage/TestimonialPage.js
+++ src/components/pages/testimonialPage/TestimonialPage.js
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Autoplay } from 'swiper';
 import 'swiper/css';
@@ -10,10 +14,30 @@ import TestimonialImg from '../../../resources/img/section_testimonials-img-min.
 import TestimonialSliderImg1 from '../../../resources/img/testimonial_feed-img1-min.png'
 import TestimonialSliderImg2 from '../../../resources/img/testimonial_feed-img2-min.png'
 import TestimonialSliderImg3 from '../../../resources/img/testimonial_feed-img3-min.png'
+import FaqImg from '../../../resources/img/section_faq-bg-min.png'
 
 import './testimonialPage.scss'
 
 const TestimonialPage = () => {
+    const [activeIndex, setActiveIndex] = useState(null);
+    const panels = [
+        {
+            title: 'Do you sell trees?',
+            content: 'Yes, in our store, you can buy decorative trees for the garden.',
+        },
+        {
+            title: 'Can I buy already grown-up plants?',
+            content: 'Of course, we have a large greenhouse where we grow plants.You can buy an adult plant in a pot.',
+        },
+        {
+            title: 'Can I buy plants online?',
+            content: "No, we don't have an online store.To buy a plant, come to our offline store or leave us a message.",
+        },
+    ];
+
+    const onAccordeonClick = (index) => {
+        setActiveIndex(index === activeIndex ? null : index);
+    };
 
     return (
         <>
@@ -45,6 +69,49 @@ const TestimonialPage = () => {
                             </div>
                             <div className="testimonial_divider_wrapper-item-descr">
                                 Call us <span><a href="tel:+1(234)5678900">+1 (234) 567 89 00</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="faq">
+                <div className="container">
+                    <div className="faq_wrapper">
+                        <div className="faq_wrapper-img">
+                            <img src={FaqImg} alt="flower" />
+                        </div>
+                        <div className="faq_wrapper-content">
+                            <span className="faq_wrapper-content-name">FAQ</span>
+                            <h2 className="faq_wrapper-content-title">Have a Question?</h2>
+                            <div className="faq_wrapper-content-descr">
+                                We are happy to answer any questions!
+                                Come to our store or leave your question in the contact form!
+                            </div>
+                            <div className="faq_wrapper-content-questions">
+                                {panels.map((panel, index) => (
+                                    <div className="faq_wrapper-content-questions-item" key={index}>
+                                        <div
+                                            className="faq_wrapper-content-questions-item-title"
+                                            onClick={() => onAccordeonClick(index)}
+                                        >
+                                        {panel.title}
+                                        <FontAwesomeIcon
+                                            icon={activeIndex === index ? faChevronUp : faChevronUp}
+                                            className={`faq_wrapper-content-questions-item-icon ${activeIndex === index
+                                            ? "faq_wrapper-content-questions-item-icon-active" : ""
+                                        }`}/>
+                                        </div>
+                                        {/* {activeIndex === index && (  */}
+                                        <div
+                                            className={`faq_wrapper-content-questions-item-descr ${activeIndex === index
+                                                ? "faq_wrapper-content-questions-item-descr-active"
+                                                : ""
+                                            }`}>
+                                            {panel.content}
+                                        </div>
+                                        {/* // )}  */}
+                                    </div>
+                                ))}
                             </div>
                         </div>
                     </div>
